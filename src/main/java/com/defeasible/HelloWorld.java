fafbaab5bc54e0f28cae0e12bf396979fafa4b7d
SwaranS <singhsw@tcd.ie>
2016-10-31 22:38:22 +0000

    Defeasible Hello

--- src/main/java/com/defeasible/HelloWorld.java
+++ src/main/java/com/defeasible/HelloWorld.java
@@ -4,4 +4,9 @@ package com.defeasible;
  * Created by Home on 10/31/2016.
  */
 public class HelloWorld {
+
+    public static void main(String[] coffee) {
+        //Say Hello
+        //Check DelpService.java
+    }
 }
